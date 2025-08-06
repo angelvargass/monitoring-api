cbe32e29c8c39ae3e458cfce304b529b859dacd1
angelvargass <59036972+angelvargass@users.noreply.github.com>
2025-08-06 10:06:17 -0600

    feat: initial endpoints

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func errorHandler(w http.ResponseWriter, r *http.Request) { //nolint:unparam,revive
+	w.WriteHeader(500)
+	w.Write([]byte("error endpoint")) //nolint:errcheck
+}
+
+func readyHandler(w http.ResponseWriter, r *http.Request) { //nolint:unparam,revive
+	w.Write([]byte("ready")) //nolint:errcheck
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) { //nolint:unparam,revive
+	w.Write([]byte("ok")) //nolint:errcheck
+}
 
 func main() {
-	fmt.Println("Generated from repository-provisioner")
+	mux := http.NewServeMux()
+	mux.Handle("/metrics", promhttp.Handler())
+	mux.HandleFunc("/error", errorHandler)
+	mux.HandleFunc("/ready", readyHandler)
+	mux.HandleFunc("/healthz", healthzHandler)
+
+	slog.Info("listening", slog.Int("port", 8080))
+	http.ListenAndServe(":8080", mux) //nolint:errcheck,gosec
 }
